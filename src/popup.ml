f033cdb1d3854c4e2a56a50e97e43dca5202139b
Anders Fugmann <anders@fugmann.net>
2025-05-14 08:46:56 +0200

    Use window storage. Not sure its retreivable

--- src/popup.ml
+++ src/popup.ml
@@ -15,26 +15,25 @@ let () =
       in
 
       let save_key (_ : Dom_html.event Js.t) =
-        let key = Js.to_string apikey_input##.value |> String.trim in
-        let _ =
-          Js.Unsafe.global##chrome##storage##sync##set
-            (Js.Unsafe.obj [| ("omdbApiKey", Js.Unsafe.inject (Js.string key)) |])
-            (Js.wrap_callback (fun _ -> show_status "API key saved!"))
-        in
+        let storage = Dom_html.window##.localStorage in
+        let key = "omdbApiKey" in
+        let value = Js.to_string apikey_input##.value |> String.trim in
+        Js.Optdef.iter storage (fun storage -> storage##setItem (Js.string key) (Js.string value));
+        show_status "API key saved!";
         Js._false
       in
 
       let load_key () =
-        let _ =
-          Js.Unsafe.global##chrome##storage##sync##get
-            (Js.array [| Js.string "omdbApiKey" |])
-            (Js.wrap_callback (fun result ->
-              let key = Js.Optdef.to_option (Js.Unsafe.get result "omdbApiKey") in
-              match key with
-              | Some k -> apikey_input##.value := Js.to_string k |> Js.string
-              | None -> ()
-            ))
+        let key = "omdbApiKey" in
+        let storage = Dom_html.window##.localStorage |> Js.Optdef.to_option in
+        let value = Option.bind storage
+            (fun storage ->
+               let x = storage##getItem (Js.string key) in
+               let y = Js.Opt.to_option x in
+               y
+            )
         in
+        Option.iter (fun value -> apikey_input##.value := value; show_status "Api key loaded") value;
         ()
       in
 
